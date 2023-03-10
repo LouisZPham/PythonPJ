49c199435dea38e34e1d0f0601a628fa311d7788
ADMIN-PC\Louis <louis.z.pham@gmail.com>
2023-03-10 14:57:21 +0700

    no message

--- Python practises/Mean and Median example.py
+++ Python practises/Mean and Median example.py
@@ -14,3 +14,15 @@ if len(list1) % 2 == 0:
 else:
     median = list1[len(list1)//2]
 print(median)
+
+#MODE
+frequency = {}
+for i in list1:
+    frequency.setdefault(i, 0)
+    frequency[i]+=1
+
+frequent = max(frequency.values())
+for i, j in frequency.items():
+    if j == frequent:
+        mode = i
+print(mode)
